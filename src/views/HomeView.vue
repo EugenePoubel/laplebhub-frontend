8b56274a4178080ea0c88b312bbea8c59a990108
Adrien Pons <adrien.pons@protonmail.com>
2026-02-05 13:08:59 +0100

    Initial frontend commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h2> Petit projet maison pour apprendre Vue.JS et Laravel </h2>
+  </main>
+</template>
